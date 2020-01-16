4f48b90533dccb422f0c5ac39673be6d779b9c40
DJ Chemik <dj-chemik@wp.pl>
2020-01-16 12:27:52 +0100

    Update packages in project

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,17 +10,21 @@ public class Main extends Application {
 
     private int windowWidth = 1280;
     private int windowHeight = 720;
+    private String title = "Event Hunter - your place to search something new in your town";
 
     public void setResolution(int width, int height) {
         windowWidth = width;
         windowHeight = height;
     }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        Parent root = FXMLLoader.load(getClass().getResource("gui/fxmlFiles/sample.fxml"));
+        primaryStage.setTitle(title);
         primaryStage.setScene(new Scene(root, windowWidth, windowHeight));
         primaryStage.show();
     }
