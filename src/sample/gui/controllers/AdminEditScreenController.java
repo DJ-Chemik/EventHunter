d2b5b16cdc8f0f7ac0353d9a166c5cf97233b704
dj-chemik <dj-chemik@wp.pl>
2020-01-19 00:41:35 +0100

    Add back buttons in EditScreen and DleteScreen

--- src/sample/gui/controllers/AdminEditScreenController.java
+++ src/sample/gui/controllers/AdminEditScreenController.java
@@ -1,4 +1,11 @@
 package sample.gui.controllers;
 
+import javafx.fxml.FXML;
+
 public class AdminEditScreenController extends Screen{
+
+    @FXML
+    public void backButtonClick(){
+        openScreenFromFXMLFilesPackage("AdminMenuScreen.fxml");
+    }
 }
