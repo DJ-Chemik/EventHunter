dc6b3304d349281554f4202c417148931674a6a0
DJ Chemik <dj-chemik@wp.pl>
2020-02-05 20:01:05 +0100

    Add sql script to project

--- src/sample/gui/controllers/UserMenuScreenController.java
+++ src/sample/gui/controllers/UserMenuScreenController.java
@@ -1,4 +1,12 @@
 package sample.gui.controllers;
 
+import javafx.fxml.FXML;
+
 public class UserMenuScreenController extends Screen {
-         }
+
+    @FXML
+    public void backButtonClick() {
+        openScreenFromFXMLFilesPackage("StartScreen.fxml");
+    }
+
+}
