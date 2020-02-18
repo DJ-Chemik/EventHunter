7ec8f054630c7d1863104eb4d7822f06d05672f6
DJ Chemik <dj-chemik@wp.pl>
2020-02-18 12:11:09 +0100

    Repair connecting with database in interface

--- src/sample/database/ConnectionWithDatabase.java
+++ src/sample/database/ConnectionWithDatabase.java
@@ -8,7 +8,7 @@ import java.sql.*;
 public class ConnectionWithDatabase {
 
     private static String DATABASE_URL;// = "jdbc:mysql://localhost:3306/projekt?useTimezone=true&serverTimezone=UTC";
-    private  static String DATABASE_USER = "root";
+    private static String DATABASE_USER = "root";
     private static String DATABASE_PASSWORD = "mcdj";
     private static String DATABASE_DRIVER = "com.mysql.cj.jdbc.Driver";
 
@@ -19,22 +19,24 @@ public class ConnectionWithDatabase {
     private static ResultSet resultSet;
 
     public ConnectionWithDatabase(String host, String port, String schemeName, String user, String password) {
-        DATABASE_USER=user;
-        DATABASE_PASSWORD=password;
-        DATABASE_URL="jdbc:mysql://" + host + ":" + port + "/" + schemeName + "?useTimezone=true&serverTimezone=UTC";
+        DATABASE_USER = user;
+        DATABASE_PASSWORD = password;
+        DATABASE_URL = "jdbc:mysql://" + host + ":" + port + "/" + schemeName + "?useTimezone=true&serverTimezone=UTC";
     }
 
-    public static Connection getConnection(){
+    public static Connection getConnection() {
         return connection;
     }
 
-    public static void connect(){
+    public static void connect() throws SQLException {
+
         try {
             Class.forName(DATABASE_DRIVER);
-            connection = DriverManager.getConnection( DATABASE_URL, DATABASE_USER, DATABASE_PASSWORD);
-        } catch (SQLException | ClassNotFoundException e) {
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+        connection = DriverManager.getConnection(DATABASE_URL, DATABASE_USER, DATABASE_PASSWORD);
+
     }
 
     public static ResultSet queryToDatabase(String sqlQuery) throws SQLException {
@@ -44,7 +46,7 @@ public class ConnectionWithDatabase {
 
     }
 
-    public static void close(){
+    public static void close() {
         try {
             connection.close();
         } catch (SQLException e) {
